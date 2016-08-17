57d67d27b19c43bd72a7f2d4811b868133c15b4a
hylide <yymyym2009new@163.com>
2016-08-17 10:59:30 +0800

    ws

--- static/js/websock-common.js
+++ static/js/websock-common.js
@@ -5,15 +5,26 @@
 * @Last Modified time: 2016-08-08 09:58:59
 */
 
-var ws_func = function (url){
-    var sock = io(url)
-    sock.on('connect', function(){
-        sock.send('hi');
+var ws = function (url) {
+    if ("WebSocket" in window) {
+        var sock = new WebSocket(url);
 
-        sock.on('message', function(msg){
-            console.log(msg);
-            sock.send('aha');
-        });
-
-    });
+        sock.onopen = function () {
+            sock.send("Login");
+            console.log("connect ...");
+        };
+        sock.onmessage = function (evt) {
+            var recv = evt.data;
+            while (True) {
+                console.log(recv);
+                sock.send("KeepAlive");
+                setInterval(5000);
+            }
+        };
+        sock.onclose = function () {
+            console.log('Connection is closed. Logout');
+        };
+    } else {
+        console.log("WebSocket NOT supported by your Browser!");
+    }
 }
